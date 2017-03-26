f22c95c227e1045349d9a163dd58b66506a36a34
vman77v <v_viayra@yahoo.com>
2017-03-26 15:14:50 -0600

    MTA LEARNING Chapter 1 work throughs

---
+++ CSharp-Learning/Learning/MTA/Chapter1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning.MTA
+{
+    class Chapter1
+    {
+        public static void Example1(int n)
+        {
+            if (n < 10)
+            {
+                Console.WriteLine("n is less than 10");
+            }
+
+            else if (n < 20)
+            {
+                Console.WriteLine("n is less than 20");
+            }
+
+            else if (n < 30)
+            {
+                Console.WriteLine("n is less than 30");
+            }
+
+            else
+            {
+                Console.WriteLine("n is greater than 30");
+            }
+        }
+
+        public static void Example2(int op1, int op2, char opr)
+        {
+            int result;
+            switch (opr)
+            {
+                case '+':
+                    result = op1 + op2;
+                    break;
+                case '-':
+                    result = op1 - op2;
+                    break;
+                case '*':
+                    result = op1 * op2;
+                    break;
+                case '/':
+                    result = op1 / op2;
+                    break;
+                default:
+                    Console.WriteLine("Unkown Operator");
+                    return;                    
+            }
+            Console.WriteLine("Result {0}", result);
+            return;
+        }
+    }
+}
--- CSharp-Learning/Learning/Program.cs
+++ CSharp-Learning/Learning/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Learning.MTA;
 
 namespace Learning
 {
@@ -10,7 +11,8 @@ namespace Learning
     {
         static void Main(string[] args)
         {            
-
+            Chapter1.Example1(10);
+            Chapter1.Example2(10, 20, '+');
         }
     }
 }
